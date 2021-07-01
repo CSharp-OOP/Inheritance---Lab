361f06bb859b5ac678bbfb763fe734179e05c67c
remzi chaushev <remzitdf@abv.bg>
2021-07-01 12:33:30 +0300

    Add project files.

---
+++ 05.StackOfString/StackOfStrings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        private Stack<string> myStack;
+
+        public StackOfStrings()
+            : base()
+        {
+            myStack = new Stack<string>();
+        }
+        public bool IsEmpty()
+        => myStack.Any();
+
+        public void AddRange(Stack<string> collection)
+        {
+            foreach (var item in collection)
+            {
+                base.Push(item);
+            }
+        }
+    }
+}
